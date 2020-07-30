aec34b9f123f2b7916aa57ecf20ce8a81f1d74ce
Prajwal Kulkarni <prajwal.kp305@gmail.com>
2020-07-30 12:21:44 +0530

    👆 Integrated Local Storage

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-// init weather object
-const weather = new Weather('Bangalore')
+// Init Storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData()
+// Init weather object
+const weather = new Weather(weatherLocation.city)
+// Init UI
 const ui = new UI()
-
 // getweather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather)
 
 
-// weather.changeLocation('Mumbai')
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value
+  weather.changeLocation(city)
+  // Set location in Local Storage
+  storage.setLocationData(city)
+  // get and display weather 
+  getWeather();
+  $('#locModal').modal('hide');
+
+})
 
 function getWeather() {
   weather.getWeather()
     .then(results => {
-      const wallpaper = document.getElementById('w-desc').innerHTML
-      
-        document.body.style.backgroundImage = "url('../img/rain.jpg')"
-      
       ui.paint(results)
     })
     .catch(
